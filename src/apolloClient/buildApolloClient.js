40cb754e7e46ac54ff4e87bb239a820c5c037744
Michael-Bryant Choa <mbchoa@gmail.com>
2018-03-21 15:59:49 -0700

    CEVICHE-418 AoR POC: Implement Edit Shipping Addres on Frontend

--- src/apolloClient/buildApolloClient.js
+++ src/apolloClient/buildApolloClient.js
@@ -1,21 +1,26 @@
+import { get, includes, invoke } from 'lodash';
+import { CREATE, DELETE, GET_LIST, GET_MANY, GET_ONE, GET_MANY_REFERENCE, QUERY_TYPES, UPDATE } from './types';
+
 import mapResourceTypeToResolver from './mapResourceTypeToResolver';
 import buildQuery from './buildQuery';
 
 export default (apolloClient) => {
   return async (type, resource, params) => {
-    const data = await apolloClient
-      .query(buildQuery(type, resource, params))
-      .then(response => response.data[mapResourceTypeToResolver(type, resource)]);
+    const data = await invoke(
+      apolloClient,
+      includes(QUERY_TYPES, type) ? 'query' : 'mutate',
+      buildQuery(type, resource, params)
+    ).then(({ data }) => data[mapResourceTypeToResolver(type, resource)]);
 
     switch (type) {
-      case 'GET_LIST':
-      case 'GET_MANY':
-      case 'GET_MANY_REFERENCE':
+      case GET_LIST:
+      case GET_MANY:
+      case GET_MANY_REFERENCE:
         return { data, total: data.length };
-      case 'GET_ONE':
-      case 'CREATE':
-      case 'UPDATE':
-      case 'DELETE':
+      case GET_ONE:
+      case CREATE:
+      case UPDATE:
+      case DELETE:
         return { data };
       default:
         throw new Error('Unsupported fetch type');
